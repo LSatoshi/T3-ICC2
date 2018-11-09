bd84d56b40c432fbaffd578b35dbe15ed80a8e79
leandrosatoshi <leandro.satoshi@usp.br>
2018-11-09 21:03:16 -0200

    added src and dice.c

---
+++ src/dice.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int sum(int *arr, int n){
+	int i, sum = 0;
+	for(i = 0; i < n; i++) sum += arr[i];
+	return(sum);
+}
+
+//n dados de m lados resultando em X
+void comb(int n, int m, int X){
+	int *dices, i, j, cont = 0;
+	dices = (int*)malloc((n+1)*sizeof(int)); //cria um vetor n+1 posicoes
+	for(i = 0; i < n; i++) dices[i] = 1; 
+	dices[n] = 0; // a posicao n eh usada para verificar se todas combinacoes ja foram feitas
+	while(dices[n] == 0){
+		if(dices[0] < m) dices[0]++;
+		else {
+			dices[0] = 1;
+			j = 1;
+			dices[j]++;
+			while(dices[j] > m){
+				dices[j] = 1;
+				j++;
+			}
+		}
+		if(sum(dices, n) == X) cont++;
+	}
+	printf("Há %d modos de se obter %d lancando %d dados com %d lados", cont, X, n, m);
+	return;
+}
+
+int main(){
+	int n=4, m=10, X=25;
+	comb(n,m,X);
+	return(0);
+}
+	
