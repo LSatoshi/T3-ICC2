2475b1ad1e06ef47e393602f511b4d454aa3f1f5
Gabriel Fontes <g.fontes@usp.br>
2018-11-20 20:31:32 -0200

    Finalizado PA

--- src/ap.c
+++ src/ap.c
@@ -1,46 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+/*
+Troca dois elementos
+*/
+void swap(int *x, int *y) {
+	int tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
 /*
 Toma o último elemento como pivô, colocando na posição correta.
 E todos os elementos à sua esquerda, e os maiores à direita
 */
 int partition (int *in, int sta, int end) { 
-    int pivot = in[end];
-    int i = (sta - 1); //Menor elemento
-  
-    for (int j = sta; j <= end - 1; j++) { 
-        // Caso seja menor ou igual ao pivô
-        if (in[j] <= pivot) { 
-            i++;
-            swap(&in[i], &in[j]); 
-        } 
-    } 
-    swap(&in[i + 1], &in[end]); 
-    return (i + 1); 
+	int pivot = in[end];
+	int i = (sta - 1); //Menor elemento
+ 
+	for (int j = sta; j <= end - 1; j++) { 
+		// Caso seja menor ou igual ao pivô
+		if (in[j] <= pivot) { 
+			i++;
+			swap(&in[i], &in[j]); 
+		} 
+	} 
+	swap(&in[i + 1], &in[end]); 
+	return (i + 1); 
 } 
-  
+ 
 /*
 Implementação do quicksort, recebe um vetor a ser desordenado, o indice de fim e o indice de inicio
 */
-void quickSort(int arr[], int low, int high) 
-{ 
-    if (low < high) 
-    { 
-        /* pi is partitioning index, arr[p] is now 
-           at right place */
-        int pi = partition(arr, low, high); 
-  
-        // Separately sort elements before 
-        // partition and after partition 
-        quickSort(arr, low, pi - 1); 
-        quickSort(arr, pi + 1, high); 
-    } 
+void quicksort(int *in, int sta, int end) { 
+	if (sta < end) { 
+		int pi = partition(in, sta, end); 
+		quicksort(in, sta, pi - 1); 
+		quicksort(in, pi + 1, end); 
+	} 
 } 
+
+
+
 
 typedef struct _ap {
 	int sta;	//Primeiro elemento da PA
 	int len;	//Tamanho da PA
 	int rat;	//Razão da PA
 } AP;
+
+/*
+Imprime uma PA na tela
+*/
+void print_ap (AP A) {
+	for (int i = 0; i < A.len; i++) {
+		printf("%d ", (A.sta + i * A.rat));
+	}
+	printf("\n");
+}
 
 /*
 Busca uma PA especifica dentro de um vetor ordenado dado
@@ -51,7 +69,7 @@ AP get_ap (int *in, int sta, int rat) {
 	int last = sta;	//Ultimo elemento encontrado até agora, auxiliar
 	int len = 1;	//Tamanho, começando a contar pelo primeiro elemento (vulgo sta)
 	
-	if (int i = 0; in[i] <= last+rat; i++) {	//Rodar até ultrapassar o proximo elemento da PA desejada
+	for (int i = 0; in[i] <= last+rat; i++) {	//Rodar até ultrapassar o proximo elemento da PA desejada
 		if (in[i] == last+rat) {				//Caso encontre
 			len++;								//Adicionar 1 no tamanho
 			last += rat;						//Atualizar o auxiliar
@@ -61,10 +79,46 @@ AP get_ap (int *in, int sta, int rat) {
 	out.sta = sta;
 	out.len = len;
 	out.rat = rat;
-	
+	//print_ap(out); //Debug
 	return (out);
 }
 
-void best_ap (int *in, int len) {
-	quickSort(in, 0, len-1);
+/*
+Retorna a melhor PA em um vetor ordenado
+*/
+AP best_ap (int *in, int len) {
+	AP best, tmp;
+	best.len = 0;
+	
+	
+	//Subtrair todos os elementos por todos os elementos, para obter todas as razões e inicios possiveis
+	for (int i = 0; i < len; i++) {
+		for (int j = 0; j < len; j++) {
+			//Encontrar a pa requisitada
+			tmp = get_ap(in, in[i], in[j]-in[i]);
+			//Caso seja maior que a anterior, atualizar a melhor pa
+			if (tmp.len > best.len) best = tmp;
+		}
+	}
+	
+	return (best);
+}
+
+
+
+
+
+
+
+
+
+int main(int argc, char **argv) {
+	int len = argc-1; //Tamanho do vetor
+	int *in = (int *)malloc((len)*sizeof(int));
+	
+	for (int i = 0; i < len; i++) {
+		in[i] = strtol(argv[i+1], NULL, 10); //Converter as strings em ints
+	}
+	quicksort(in, 0, len-1); //Ordenar a entrada
+	print_ap(best_ap(in, len)); //printar a melhor pa
 }
